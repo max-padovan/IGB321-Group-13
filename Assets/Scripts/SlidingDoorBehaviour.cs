1abf834da8a97224f11128cbc8db9be633201fe8
Stewding1990 <91507265+Stewding1990@users.noreply.github.com>
2023-10-20 15:04:03 +1000

    UPdated tiles, added in a door script

---
+++ Assets/Scripts/SlidingDoorBehaviour.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorBehaviour : MonoBehaviour
+{
+    public Transform doorTransform;
+    public bool isOpen = false;
+    public float slideSpeed = 2.0f;
+    public GameObject interactionPanel;
+    public GameObject player;
+    private Vector3 openPosition;
+    private Vector3 closedPosition;
+
+    // Initialize the open and closed positions
+    private void Start()
+    {
+        closedPosition = doorTransform.position;
+        openPosition = closedPosition + new Vector3(30, 0, 0);
+        HideInteractionPanel();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Check for player interaction (e.g., pressing a switch)
+        if (isplayerCloseEnoughttoPanel())
+        {
+            ShowInteractionPanel();
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isOpen = !isOpen;
+            }
+        }
+        else
+        {
+            HideInteractionPanel();
+        }
+
+        // Interpolate the door's position to create a smooth sliding effect
+        if (isOpen)
+        {
+            doorTransform.position = Vector3.Lerp(doorTransform.position, openPosition, Time.deltaTime * slideSpeed);
+        }
+        else
+        {
+            doorTransform.position = Vector3.Lerp(doorTransform.position, closedPosition, Time.deltaTime * slideSpeed);
+        }
+    }
+
+    private bool isplayerCloseEnoughttoPanel()
+    {
+        return (Vector3.Distance(player.transform.position, interactionPanel.transform.position) < 5.0f);
+    }
+
+    private void ShowInteractionPanel()
+    {
+        interactionPanel.SetActive(true);
+    }
+
+    private void HideInteractionPanel()
+    {
+        interactionPanel.SetActive(false);
+    }
+}
