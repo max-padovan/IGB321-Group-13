0edf64e4a0ef056363f152ebf9a377047a1b89e0
Max Padovan <122199194+max-padovan@users.noreply.github.com>
2023-10-29 03:09:58 +1000

    Default doors

--- Assets/Scripts/MaxDoor.cs
+++ Assets/Scripts/MaxDoor.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class MaxDoor : MonoBehaviour
 {
-    private GameObject door;
-    public GameObject player;
-    public float openTime = 2.0f;
-    public Vector3 endPos;
-    private Vector3 startPos;
+    private GameObject player;
+    private Transform door;
+    private Vector3 openPos, closedPos;
+    public float speed, distanceToDoor;
+    private bool doorIsOpen;
 
     void Start()
     {
-        door = GameObject.Find("Door");
-        startPos = door.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        door = transform.Find("Door");
+
+        closedPos = new Vector3(0f, 0f, 0f);
+        openPos = new Vector3(0f, -10f, 0f);
+
+        doorIsOpen = false;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        if (Vector3.Distance(player.transform.position, transform.position) < distanceToDoor && doorIsOpen == false) //if your close enough to the door
         {
-            door.transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime);
+            if (door.transform.position.y >= openPos.y) //if the door is greater than the open pos
+            {
+                door.transform.Translate(0, -speed * Time.deltaTime, 0); //move the door down
+            }
+
+            if (door.transform.position.y <= openPos.y) //if the door is at the open pos
+            {
+                doorIsOpen = true;
+            }
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) > distanceToDoor && doorIsOpen == true)
+        {
+            if (door.transform.position.y <= closedPos.y) // if the door is less than the closed position
+            {
+                door.transform.Translate(0, speed * Time.deltaTime, 0); //move the door up
+            }
+
+            if (door.transform.position.y >= closedPos.y) //if the door is at the closed pos
+            {
+                doorIsOpen = false;
+            }
         }
     }
 }
